894a1f983e39c1bae9dd05952b16685a683c7706
Gokhan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-02-12 23:16:38 +0300

    Initial Commit

---
+++ GitLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitLearning
+//
+//  Created by Gokhan Bozkurt on 12.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Gokhan Bozkurt on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
